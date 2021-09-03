b5d94125b98e144eca8cc96a41004efce6765180
ubaidillah1 <ubayyubaid@gmail.com>
2021-09-03 11:33:46 +0700

    y

--- plugins/callback.py
+++ plugins/callback.py
@@ -216,8 +216,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/ChannelTrident'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)

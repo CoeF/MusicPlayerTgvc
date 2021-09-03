76c96233f11aeb3f44889e21ada58903d832a16f
ubaidillah1 <ubayyubaid@gmail.com>
2021-09-03 11:26:45 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
-cd /MusicPlayer
+git clone https://github.com/qowwim-lab/MusicPlayerTgvc.git /MusicPlayerTgvc
+cd /MusicPlayerTgvc
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
